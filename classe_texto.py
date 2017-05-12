b22123643f583cfbf07af0075cf73454c94dfc87
Aninha....... B....B....H <Aninha....... B....B....H>
2017-05-12 11:34:24 -0300

    Classe de texto com contador consertado

--- classe_texto.py
+++ classe_texto.py
@@ -3,11 +3,11 @@ class texto:
         self.textos=textos
         self.dimensoes1=dimensoes1
         self.dimensoes2=dimensoes2
-    def novo_blit(self,fonte,cor,screen):
+    def novo_blit(self,fonte,cor,screen,espaço):
         cont=0
         for texto in self.textos:
             import pygame
-            cont=cont+10
+            cont=cont+espaço
             t=fonte.render(texto,1,cor) 
             screen.blit((t),(self.dimensoes1,self.dimensoes2+cont))
             pygame.display.update()
