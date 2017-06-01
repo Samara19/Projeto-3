95ea516fd4c24e029af83838ecf36116b9afafd2
Tainara <tainarasm98@outlook.com>
2017-06-01 17:02:59 -0300

    'atualizacoes'

--- saturno.py
+++ saturno.py
@@ -7,23 +7,15 @@ from classe import texto
 p.init()
 
 
-screen = p.display.set_mode((1050, 712))
-p.display.set_caption('Júpiter')
+screen = p.display.set_mode((950, 712))
+p.display.set_caption('Saturno')
 astronauta = p.image.load('animaok.gif').convert()
 astronauta_position = [680, 20]
-para_cima = 150
-para_frente = 15
-sem_pressionar = 8
 
-esquerda = p.image.load('seta_esquerda.jpg').convert()
-espaço = p.image.load('space_bar.jpg').convert()
 
-primeira_tela = p.image.load('jupiter.jpg').convert()
-segunda_tela = p.image.load('surface_jupiter.jpg').convert()
 
-rect_astronauta = astronauta.get_rect()
-rect_astronauta.right = 1000
-rect_astronauta.top = 5
+background = p.image.load('saturn.jpg').convert()
+
 
 clock = p.time.Clock()
 
@@ -46,22 +38,19 @@ while jogo == 0:
     for event in events:
         if event.type == QUIT:
             exit()
-    screen.blit(primeira_tela, (0, 0)) #coloca a imagem no display
+    screen.blit(background, (0, 0)) #coloca a imagem no display
     screen.blit(astronauta, (astronauta_position))
     #criando textos
-    tela_1 = ["Olá astronauta!", "Seja bem vindo(a) a Júpiter, o gigante do Sistema Solar!", "A minha gravidade é insana,", "então, prepare-se para flutuar por minha superfície!"]
-    instructions = ["INSTRUÇÕES:", "A sua missão é atravessar a minha superfície!", "MAS NÃO ENCOSTE NAS BORDAS SUPERIOR OU INFERIOR DA TELA!", "Utilize                   para conseguir voltar para a sua nave!", "E                                                         para se manter flutuando!", "Está preparado(a)? Se sim, pressione ENTER para avançar e boa sorte!"]
-    ganhou = ["UOOOOOOOU", "É ISSO AÍ AMIGÃO/AMIGONA!", "VOCÊ CONSEGUIU ESCAPAR DO MEU CAMPO MAGNÉTICO MONSTRUOSO!", "E VOLTAR PARA A SEGURANÇA DA SUA NAVE!", "MISSÃO CUMPRIDA COM SUCESSO!"]
+    tela_1 = ["Olá, astronauta! Seja bem vindo...", "...ao segundo maior planeta do Sistema Solar!", "Para conseguir passar por mim,", "você terá de provar ser muito bom ou boa em Matemática!", "E olha que não é fácil não, hein?", "Está preparado para começar?", "Se sim, pressione ENTER para avançar!"] 
+    tela_2 = ["Pois bem, vamos começar:", "Diga-me, quanto é 15 X 4?", "Poooxa, é fácil, vamos lá, eu acredito em você!", "Digite 1 para 50", "Digite 2 para 45", "Digite 3 para 90", "Ou 4 para 60"]
+    ganhou = ["UOOOOOOOU", "É ISSO AÍ AMIGÃO/AMIGONA!", "VOCÊ CONSEGUIU ESCAPAR DO MEU CAMPO MAGNÉTICO MONSTRUOSO!", "E VOLTAR PARA A SEGURANÇA DA SUA NAVE!" "MISSÃO CUMPRIDA COM SUCESSO!"]
     perdeu = ["GAME OVER!"]
-
+   
    #inserindo textos no display
-
     entrada = texto(tela_1,95, 20)
     entrada.novo_blit(fonte_jogo, WHITE, screen, 40)
-    entrada_2 = texto(instructions, 95, 200)
-    entrada_2.novo_blit(fonte_jogo, WHITE, screen, 60)
-    screen.blit(esquerda, (180, 400))
-    screen.blit(espaço, (120, 485))
+    #entrada_2 = texto(instructions, 95, 200)
+    #entrada_2.novo_blit(fonte_jogo, WHITE, screen, 40)
 
     
 
@@ -74,8 +63,14 @@ while jogo == 0:
         for event in events:
             if event.type == p.KEYDOWN:
                 if event.key == p.K_ESCAPE:
+                    jogo = 1
                     p.display.quit()
                 if event.key == p.K_RETURN:
+                    screen.fill(BLACK)
+                    screen.blit(segunda_tela, (0,0))
+                    screen.blit(astronauta, rect_astronauta)
+                    p.display.update()
+                    time_passed = clock.tick(30)
                     jogo=1
             if jogo==1:
                 if event.type == p.KEYDOWN:
@@ -84,9 +79,6 @@ while jogo == 0:
                     if event.key==p.K_SPACE:
                         screen.fill(BLACK)
                         screen.blit(segunda_tela, (0,0))
-                        p.draw.line(screen, (255, 0, 0), (0, 356), (1050, 356), 5)
-                        if rect_astronauta.top>250:
-                            para_cima=50
                         rect_astronauta.top-=para_cima
                         screen.blit(astronauta, rect_astronauta)
                         p.display.update()
@@ -94,7 +86,6 @@ while jogo == 0:
                     if event.key==p.K_LEFT:
                         screen.fill(BLACK)
                         screen.blit(segunda_tela, (0,0))
-                        p.draw.line(screen, (255, 0, 0), (0, 356), (1050, 356), 5)
                         rect_astronauta.right-=para_frente
                         screen.blit(astronauta, rect_astronauta)
                         p.display.update()
@@ -103,35 +94,31 @@ while jogo == 0:
         if jogo==1:
             screen.fill(BLACK)
             screen.blit(segunda_tela, (0,0))
-            p.draw.line(screen, (255, 0, 0), (0, 356), (1050, 356), 5)
             rect_astronauta.top+=sem_pressionar
             screen.blit(astronauta, rect_astronauta)
             p.display.update()
             time_passed = clock.tick(30)
+            p.display.update()
+            time_passed = clock.tick(30)
             if rect_astronauta.top<5:
                 jogo=2
-            if rect_astronauta.top>420:
+            if rect_astronauta.top>400:
                 jogo=2
-            if rect_astronauta.left<-270:
+            if rect_astronauta.left>150:
                 jogo=3
 
+
         if jogo==2:
-            screen.fill(BLACK)
-            screen.blit(segunda_tela, (0,0))
             screen.blit(astronauta, rect_astronauta)
-            entrada = texto(perdeu,280, 320)
+            entrada = texto(perdeu,255, 320)
             entrada.novo_blit(fonte_perdeu, WHITE, screen, 0)
             p.display.update()
             time_passed = clock.tick(30)
-            p.time.delay(3000)
-            p.display.quit()
 
         if jogo==3:
-            screen.fill(BLACK)
-            screen.blit(segunda_tela, (0,0))
             screen.blit(astronauta, rect_astronauta)
-            entrada = texto(ganhou,50, 230)
-            entrada.novo_blit(fonte_jogo, WHITE, screen, 40)
+            entrada = texto(ganhou,255, 320)
+            entrada.novo_blit(fonte_jogo, WHITE, screen, 0)
             p.display.update()
             time_passed = clock.tick(30)
 
---
+++ venus.py
@@ -0,0 +1,245 @@
+import pygame as p
+from pygame.locals import* 
+from sys import exit
+from os import system
+from classe import texto
+
+p.init()
+
+
+screen = p.display.set_mode((1200, 760))
+p.display.set_caption('Vênus')
+astronauta = p.image.load('animaok.gif').convert()
+astronauta_position = [900, 20]
+
+
+BLACK = (0,0,0)
+WHITE = (255, 255, 255)
+
+
+
+primeira_tela = p.image.load('venus.jpg')
+segunda_tela = p.image.load('ceu_venus.jpg').convert()
+terceira_tela = p.image.load('surface_venus.png').convert()
+x = 1200
+rect_branco = p.image.load('white_rect.png').convert()
+robo = p.image.load('curiosity_png.png').convert_alpha()
+rect_robo = robo.get_rect()
+rect_robo.right = 600
+rect_robo.top = 460
+bigorna = p.image.load('bigorna.png').convert_alpha()
+rect_bigorna = bigorna.get_rect()
+rect_bigorna.top = 0
+rect_bigorna.right = 500
+fogo = p.image.load('fogo.png').convert()
+rect_fogo = fogo.get_rect()
+rect_fogo.top = 300
+rect_fogo.right = 500
+
+
+clock = p.time.Clock()
+
+p.font.init()
+
+fonte_padrao = p.font.get_default_font()
+fonte_jogo = p.font.SysFont(fonte_padrao, 30)
+fonte_ganhou = p.font.SysFont(fonte_padrao, 50)
+fonte_perdeu = p.font.SysFont(fonte_padrao, 100)
+fonte_novoperdeu = p.font.SysFont(fonte_padrao, 60)
+
+para_frente = 20
+para_tras = 5
+para_cima = 10
+para_baixo = 10
+sem_pressionar = 5
+pular = 30
+
+
+
+
+events = p.event.get()
+
+jogo = 0
+
+while jogo == 0:
+    for event in events:
+        if event.type == p.KEYDOWN:
+                if event.key == p.K_ESCAPE:
+                    p.display.quit()
+    screen.blit(primeira_tela, (0, 0)) #coloca a imagem no display
+    screen.blit(astronauta, (astronauta_position))
+    #criando textos
+    tela_1 = ["Olá, astronauta! Meu nome é Vênus...", "...e eu sou o planeta mais quente do Sistema Solar...", "...além de ser um dos astros mais brilhantes no céu noturno aí da Terra!", "Só perco para a Lua!", "Para conseguir passar por mim,", "sua primeira missão é acertar quem sou eu no céu noturno!", "Pressione ENTER para avançar!"] 
+    tela_2 = ["INSTRUÇÕES", "Clique sobre o ponto no céu...,", "...o qual você acredita ser o planeta Vênus!"]
+    ganhou = ["É ISSO AÍ AMIGÃO/AMIGONA!"] 
+    ganhou1 = ["ESTE SOU EU MESMO!"]
+    ganhou2 = ["PRESTE MAIS ATENÇÃO EM MIM..."]
+    ganhou3 = ["...DA PRÓXIMA VEZ QUE OLHAR PARA O CÉU!"] 
+    ganhou4 = ["MISSÃO CUMPRIDA COM SUCESSO!"]
+    perdeu1 = ["Este não sou eu!"]
+    perdeu2 = ["Foi por pouco!!! Esta é a Lua, o satélite natural da Terra..."]
+    perdeu2_continue = ["...e o astro mais luminoso do céu noturno!"]
+    perdeu3 = ["Foi por pouco!!! Este é o planeta Júpiter!"]
+    perdeu3_continue = ["Que, às vezes, pode ser visto da Terra a olho nu!"]
+    perdeu0 = ["GAME OVER!"]
+    tela_redirecionamento = ["Você venceu a primeira etapa do jogo!", "Para passar por mim, ainda tem de cumprir mais uma etapa!", "Guie o robô..."]
+   
+   #inserindo textos no display
+    entrada = texto(tela_1,95, 20)
+    entrada.novo_blit(fonte_jogo, WHITE, screen, 40)
+    #p.draw.rect(screen, (150, 400, 100, 200))
+    entrada_2 = texto(tela_2, 95, 300)
+    entrada_2.novo_blit(fonte_jogo, WHITE, screen, 60)
+
+
+    
+
+    p.display.update()
+    time_passed = clock.tick(30)
+
+    jogo=0
+    
+    
+    while True:
+        events = p.event.get()
+        for event in events:
+            if jogo==0:
+                if event.type == p.KEYDOWN:
+                    if event.key == p.K_ESCAPE:
+                        p.display.quit()
+                    if event.key == p.K_RETURN:
+                        screen.fill(BLACK)
+                        screen.blit(segunda_tela, (0,0))
+                        rect_branco.set_alpha(0)
+                        screen.blit(rect_branco, (840, 248))
+                        screen.blit(rect_branco, (760, 303))
+                        screen.blit(rect_branco, (800, 348))
+                        p.display.update()
+                        time_passed = clock.tick(30)
+                if event.type == p.MOUSEBUTTONDOWN:
+                    position = p.mouse.get_pos()
+                    if 760+30>position[0]>760 and 303+30>position[1]>303:
+                        entrada = texto(ganhou,350, 100)
+                        entrada.novo_blit(fonte_ganhou, WHITE, screen, 40)
+                        entrada = texto(ganhou1,403, 200)
+                        entrada.novo_blit(fonte_ganhou, WHITE, screen, 40)
+                        entrada = texto(ganhou2,320, 300)
+                        entrada.novo_blit(fonte_ganhou, WHITE, screen, 40)
+                        entrada = texto(ganhou3,230, 400)
+                        entrada.novo_blit(fonte_ganhou, WHITE, screen, 40)
+                        entrada = texto(ganhou4,290, 500)
+                        entrada.novo_blit(fonte_ganhou, WHITE, screen, 40)
+                        p.time.delay(5000)
+                        screen.fill(BLACK)
+                        screen.blit(primeira_tela, (0, 0))
+                        entrada = texto(tela_redirecionamento,95, 20)
+                        entrada.novo_blit(fonte_jogo, WHITE, screen, 40)
+                        p.time.delay(3000)
+                        jogo = 1
+                    elif 840+30>position[0]>840 and 248+30>position[1]>248:
+                        entrada = texto(perdeu2,30, 100)
+                        entrada.novo_blit(fonte_novoperdeu, WHITE, screen, 60)
+                        entrada = texto(perdeu2_continue,150, 200)
+                        entrada.novo_blit(fonte_novoperdeu, WHITE, screen, 60)
+                        entrada = texto(perdeu0,350, 350)
+                        entrada.novo_blit(fonte_perdeu, WHITE, screen, 40)
+                        p.time.delay(4000)
+                        p.display.quit()
+                    elif 800+30>position[0]>800 and 348+30>position[1]>348:
+                        entrada = texto(perdeu3,170, 100)
+                        entrada.novo_blit(fonte_novoperdeu, WHITE, screen, 60)
+                        entrada = texto(perdeu3_continue,100, 200)
+                        entrada.novo_blit(fonte_novoperdeu, WHITE, screen, 60)
+                        entrada = texto(perdeu0,350, 350)
+                        entrada.novo_blit(fonte_perdeu, WHITE, screen, 40)
+                        p.time.delay(4000)
+                        p.display.quit()
+                    else:
+                        entrada = texto(perdeu1,305, 200)
+                        entrada.novo_blit(fonte_perdeu, WHITE, screen, 40)
+                        entrada = texto(perdeu0,350, 300)
+                        entrada.novo_blit(fonte_perdeu, WHITE, screen, 40)
+                        p.time.delay(4000)
+                        p.display.quit()
+
+                        
+            
+                    #if position[0]==835 and position[1]==245:
+                        #jogo=2
+                        #jogo = 1
+            if jogo==1:
+                if event.type == p.KEYDOWN:
+                    if event.key == p.K_ESCAPE:
+                        p.display.quit()
+                    """if event.key == p.K_RIGHT:
+                        screen.fill(BLACK)
+                        screen.blit(terceira_tela, (0,0))
+                        rect_robo.right-=para_tras
+                        screen.blit(robo, rect_robo)
+                        p.display.update()
+                        time_passed = clock.tick(30)
+                    if event.key == p.K_RIGHT:
+                        screen.fill(BLACK)
+                        screen.blit(terceira_tela, (0,0))
+                        rect_robo.right+=para_frente
+                        screen.blit(robo, rect_robo)
+                        p.display.update()
+                        time_passed = clock.tick(30)
+                    if event.key == p.K_UP:
+                        screen.fill(BLACK)
+                        screen.blit(terceira_tela, (0,0))
+                        if rect_robo.top>80:
+                            rect_robo.top-=para_cima
+                            screen.blit(robo, rect_robo)
+                            p.display.update()
+                            time_passed = clock.tick(30)
+                    if event.key == p.K_DOWN:
+                        screen.fill(BLACK)
+                        screen.blit(terceira_tela, (0,0))
+                        if rect_robo.top<330:
+                            rect_robo.top+=para_baixo
+                            screen.blit(robo, rect_robo)
+                            p.display.update()
+                            time_passed = clock.tick(30)
+                    if event.key == p.K_SPACE:
+                        screen.fill(BLACK)
+                        screen.blit(terceira_tela, (0,0))
+                        if rect_robo.top>80:
+                            rect_robo.top-=pular
+                            screen.blit(robo, rect_robo)
+                            p.display.update()
+                            time_passed = clock.tick(30)"""
+
+        if jogo==1:
+            screen.fill(BLACK)
+            #rect_terceiratela.right-=sem_pressionar
+            rel_x = x % terceira_tela.get_rect().width
+            screen.blit(terceira_tela, (rel_x-terceira_tela.get_rect().width,398))
+            if rel_x<1200:
+                screen.blit(terceira_tela, (rel_x, 398))
+            x-=sem_pressionar
+            rect_bigorna.top+=sem_pressionar
+            screen.blit(bigorna, rect_bigorna)
+            screen.blit(fogo, rect_fogo)
+            #if rect_terceiratela.right==20
+            #rect_robo.right+=sem_pressionar
+            screen.blit(robo, rect_robo)
+            p.display.update()
+            time_passed = clock.tick(30)
+            if rect_robo.right>1240:
+                screen.fill(BLACK)
+                rect_robo.right = 550
+                screen.blit(robo, rect_robo)
+
+
+        if jogo==2:
+            entrada = texto(ganhou,95, 20)
+            entrada.novo_blit(fonte_jogo, WHITE, screen, 40)
+                
+
+            
+
+
+            
+
+        
